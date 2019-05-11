52ca2a5976833dbe89d26d049a0d042558d24874
patiencedaur <patiencedaur@gmail.com>
2019-05-11 14:28:56 +0300

    Edit some meta

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // The name of your game, no spaces or special characters.
-const name = 'Monogatari';
+const name = 'PreventViolence';
 
 // The version of the cache, changing this will force everything to be cached
 // again.
