2361e0cf3809a66d53807cc4712374cb4b040b50
patiencedaur <patiencedaur@gmail.com>
2019-05-11 17:14:29 +0300

    Fix mobile appearance

--- dist/js/options.js
+++ dist/js/options.js
@@ -80,7 +80,7 @@ monogatari.settings({
 	// to portrait or landscape, a warning message will be displayed so the
 	// player rotates its device.
 	// Possible values: any, portrait or landscape.
-	'Orientation': 'landscape',
+	'Orientation': 'any',
 
 	// Allow players to skip through the game. Similar to the auto play feature,
 	// skipping will allow players to go through the game really fast.
@@ -130,7 +130,7 @@ monogatari.settings({
 monogatari.preferences ({
 
 	// Initial Language for Multilanguage Games or for the Default GUI Language.
-	'Language': 'English',
+	'Language': 'Русский',
 
 	// Initial Volumes from 0.0 to 1.
 	'Volume': {
