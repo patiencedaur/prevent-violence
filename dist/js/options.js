61640bd2cf209173d0c96bbb939b8d38e7bb385d
patiencedaur <patiencedaur@gmail.com>
2019-05-13 20:04:41 +0300

    Change version

--- dist/js/options.js
+++ dist/js/options.js
@@ -20,7 +20,7 @@ monogatari.settings({
 	'Name': 'Куда движутся отношения',
 
 	// The version of your game in semantic versioning (https://semver.org/).
-	'Version': '0.1.0',
+	'Version': '0.2.4',
 
 	// Initial Label *
 	'Label': 'Start',
