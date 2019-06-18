c2898087396291ed6b9c7ebf0ccc3d95d5729873
patiencedaur <patiencedaur@gmail.com>
2019-06-18 21:24:37 +0300

    Remove text and image animation

--- dist/js/options.js
+++ dist/js/options.js
@@ -26,7 +26,7 @@ monogatari.settings({
 	'Label': 'Start',
 
 	// Number of AutoSave Slots
-	'Slots': 10,
+	'Slots': 1,
 
 	// Change to true for a MultiLanguage GameScreen.
 	'MultiLanguage': false,
@@ -60,21 +60,21 @@ monogatari.settings({
 	'ForceAspectRatio': 'Global',
 
 	// Enables or disables the typing text animation for the whole game.
-	'TypeAnimation': true,
+	'TypeAnimation': false,
 
 	// Enables or disables the typing text animation in NVL dialogs for the
 	// whole game.
-	'NVLTypeAnimation': true,
+	'NVLTypeAnimation': false,
 
 	// Enables or disables the typing animation for the narrator.
 	// If the previous property was set to false, the narrator won't shown
 	// the animation even if this is set to true.
-	'NarratorTypeAnimation': true,
+	'NarratorTypeAnimation': false,
 
 	// Enables or disables the typing animation for the special centered
 	// character. If the TypeAnimation property was set to false, the centered
 	// character won't shown the animation even if this is set to true.
-	'CenteredTypeAnimation': true,
+	'CenteredTypeAnimation': false,
 
 	// Force some orientation on mobile devices. If this setting is set either
 	// to portrait or landscape, a warning message will be displayed so the
